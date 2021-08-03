af0f816ee59880a9ed3afa7038576a706ce036c8
biostunt <nice.nikita99@mail.ru>
2021-08-03 12:01:43 +0300

    add swagger ui to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-
 const bootstrap = async () => {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.useGlobalPipes(
@@ -11,7 +11,16 @@ const bootstrap = async () => {
             transform: true,
         }),
     );
+    const config = new DocumentBuilder()
+        .setTitle("CodeMark backend")
+        .setDescription(
+            "Backend API that allows you to manipulate with roles and users",
+        )
+        .setVersion("1.0")
+        .addTag("codemark")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
     await app.listen(3000);
 };
-
 bootstrap();
